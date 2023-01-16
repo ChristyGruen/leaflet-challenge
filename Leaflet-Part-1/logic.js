e7038eaaf0d0b0800dcf3f17a86605c5129a0b81
ChristyGruen <chrisgruen@yahoo.com>
2023-01-16 15:13:47 -0600

    added radius by magnitude

--- Leaflet-Part-1/logic.js
+++ Leaflet-Part-1/logic.js
@@ -13,98 +13,99 @@ d3.json(url).then(function (data) {
   // Once we get a response, send the data.features object to the createFeatures function.
   console.log(data.features);
 
-//create circle markers
-const circleMarkers = [];
-  data.features.forEach(feature=> {
+  //create circle markers
+  const circleMarkers = [];
+    data.features.forEach(feature=> {
 
-    circleMarkers.push(L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],
-      opacity = .9,
-      color = 'green',
-      //feature.geometry.coordinates[2],
-      fillColor ='red',
-      // feature.geometry.coordinates[2],
-      radius = 1000000
-      //feature.properties.mag,
-      ))
-      // .bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
+      let color 
 
-    });
-    console.log(circleMarkers)
+      circleMarkers.push(L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]], {
+        opacity: .9,
+        color: feature.geometry.coordinates[2],
+        fillColor: feature.geometry.coordinates[2],
+        radius: feature.properties.mag*2,
+      }))
+        // circleMarkers.setRadius(feature.properties.mag*1000)
+        // .bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
+
+      });
+      console.log(circleMarkers)
   createFeatures(data.features);
   
 
 
-function createMap(earthquakes) {
-  let circleLayer = L.layerGroup(circleMarkers);
+  function createMap(earthquakes) {
+    let circleLayer = L.layerGroup(circleMarkers);
+    console.log(circleLayer)
 
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
+    // Create the base layers.
+    let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    })
 
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+    let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
 
-  // Create a baseMaps object.
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
+    // Create a baseMaps object.
+    let baseMaps = {
+      "Street Map": street,
+      "Topographic Map": topo
+    };
+
+    // Create an overlay object to hold our overlay.
+    let overlayMaps = {
+      CircleCities:circleLayer,
+      Earthquakes: earthquakes
+      // ,Tectonics: tectonics
+    };
+
+    // Create our map, giving it the streetmap and earthquakes layers to display on load.
+    let myMap = L.map("map", {
+      center: [
+        0,0
+      ],
+      zoom: 2,
+      layers: [street,
+        circleLayer, 
+        earthquakes
+      // ,tectonics
+    ]
+    });
+
+    // Create a layer control.
+    // Pass it our baseMaps and overlayMaps.
+    // Add the layer control to the map.
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+
   };
 
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    CircleCities:circleLayer,
-    Earthquakes: earthquakes
-    // ,Tectonics: tectonics
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
-    center: [
-      0,0
-    ],
-    zoom: 2,
-    layers: [street,
-      circleLayer, 
-      earthquakes
-    // ,tectonics
-  ]
-  });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-}
 
 
 
 
 
+  function createFeatures(earthquakeData) {
 
-function createFeatures(earthquakeData) {
+    // Define a function that we want to run once for each feature in the features array.
+    // Give each feature a popup that describes the place and time of the earthquake.
+  
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
 
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
- 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
+    function onEachFeature(feature, layer) {
+      layer.bindPopup(`<h1>${feature.properties.place}</h1><hr><h3>Magnitude ${feature.properties.mag}<\h3><hr><h3>Depth ${feature.geometry.coordinates[2]}`);
+    }
 
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    var earthquakes = L.geoJSON(earthquakeData, {
+      onEachFeature: onEachFeature
+    });
+
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
   }
-
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-}
 
 //end of d3
 });
