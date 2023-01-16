7ddb436b7e326627fbd6468c6a2bf0c04f55be5b
ChristyGruen <chrisgruen@yahoo.com>
2023-01-16 14:46:15 -0600

    circle markers created, no color size yet

--- Leaflet-Part-1/logic2.js
+++ Leaflet-Part-1/logic2.js
@@ -2,73 +2,39 @@
 //C:\Users\chrisgru\UofM-VIRT-DATA-PT-09-2022-U-LOLC\15-Mapping-Web\1\Activities\10-Stu_GeoJson\Solved
 
 // Store our API endpoint as queryUrl.
-const url = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-14&endtime=2023-01-15";
+let queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2023-01-14&endtime=2023-01-15";
 // &maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
-const dataPromise = d3.json(url);
-console.log("Data Promise: ",dataPromise);
-
-// Perform a GET request to the query URL/
-d3.json(url).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  console.log(data.features);
-
-//create circle markers
-const circleMarkers = [];
-  data.features.forEach(feature=> {
-
-    circleMarkers.push(L.circleMarker([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],
-      opacity = .9,
-      color = 'green',
-      //feature.geometry.coordinates[2],
-      fillColor ='red',
-      // feature.geometry.coordinates[2],
-      radius = 1000000
-      //feature.properties.mag,
-      ))
-      // .bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`)
-
-    });
-    console.log(circleMarkers)
-  createFeatures(data.features);
-  
-
-
 function createMap(earthquakes) {
-  let circleLayer = L.layerGroup(circleMarkers);
-
   // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   })
 
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  var topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
   });
 
   // Create a baseMaps object.
-  let baseMaps = {
+  var baseMaps = {
     "Street Map": street,
     "Topographic Map": topo
   };
 
   // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    CircleCities:circleLayer,
+  var overlayMaps = {
     Earthquakes: earthquakes
-    // ,Tectonics: tectonics
+    //,Tectonics: tectonics
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
+  var myMap = L.map("map", {
     center: [
       0,0
     ],
     zoom: 2,
-    layers: [street,
-      circleLayer, 
-      earthquakes
-    // ,tectonics
+    layers: [street, earthquakes
+    //,tectonics
   ]
   });
 
@@ -82,39 +48,18 @@ function createMap(earthquakes) {
 }
 
 
-
-
-
-
-function createFeatures(earthquakeData) {
-
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
- 
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
-
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-}
-
-//end of d3
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+  // Once we get a response, send the data.features object to the createFeatures function.
+  // createFeatures(data.features);
+  console.log(data.features)
 });
 
 
-
-// function createFeatures(earthquakeData) {
-//   L.circle([-122.2705002, 37.894001]).addTo(myMap)
-//   createMap(earthquakes)
-// };
+function createFeatures(earthquakeData) {
+  // L.circle([-122.2705002, 37.894001]).addTo(myMap)
+  createMap(earthquakes)
+};
 
   // Define a function that we want to run once for each feature in the features array.
   // Give each feature a popup that describes the place and time of the earthquake.
--- README.md
+++ README.md
@@ -8,9 +8,7 @@ xxx
 ---
 ## **Purpose**
 
- In this challenge, you have been tasked with developing a way to visualize USGS data that will allow them to better educate the public and other government 
-organizations (and hopefully secure more funding) on issues facing our planet.
-You will create an earthquake visualization map using USGS GeoJSON data.  As a bonus learning opportunity, you may gather and plot more data to illustrate the relationship between tectonic plates and seismic activity.
+The purpose of this challenge is to visualize USGS data to better educate the public and other government organizations (and hopefully secure more funding) on issues facing our planet. Visualizations should include an earthquake visualization map using USGS GeoJSON data and may include additional data to illustrate the relationship between tectonic plates and seismic activity.
 
 
 ## **Attribution**
@@ -82,14 +80,19 @@ The instructions for this activity are broken into two parts:
 * Part 2: Gather and Plot More Data (Optional with no extra points earning)
 
 ###  <span style="color:tan">  **Part 1: Create the Earthquake Visualization** </span>
-<!-- add earthquake visualization here -->
+
+![Alt text](https://static.bc-edx.com/data/dl-1-1/m15/lms/img/2-BasicMap.jpg "Earthquake Map")
+
 Your first task is to visualize an earthquake dataset. Complete the following steps:
 1. Get your dataset. To do so, follow these steps:
-    * The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the USGS GeoJSON Feed Links to an external site.page and choose a dataset to visualize. The following image is an example screenshot of what appears when you visit this link:
-    <!-- 3JSON add usgs geojson page pic here -->
+    * The USGS provides earthquake data in a number of different formats, updated every 5 minutes. Visit the  <a href = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php" target = "_blank"> USGS GeoJSON Feed </a> page and choose a dataset to visualize. The following image is an example screenshot of what appears when you visit this link:
+    
+    ![Alt text](https://static.bc-edx.com/data/dl-1-1/m15/lms/img/3-Data.jpg "USGS GeoJSON Feed main page")
 
     * When you click a dataset (such as "All Earthquakes from the Past 7 Days"), you will be given a JSON representation of that data. Use the URL of this JSON to pull in the data for the visualization. The following image is a sampling of earthquake data in JSON format:
-    <!-- 4-JSON add json data pic here -->
+
+     ![Alt text](https://static.bc-edx.com/data/dl-1-1/m15/lms/img/4-JSON.jpg "USGS GeoJSON data")
+     
 2. Import and visualize the data by doing the following:
     * Using Leaflet, create a map that plots all the earthquakes from your dataset based on their longitude and latitude.
         * Your data markers should reflect the magnitude of the earthquake by their size and the depth of the earthquake by color. Earthquakes with higher magnitudes should appear larger, and earthquakes with greater depth should appear darker in color.
@@ -106,7 +109,9 @@ This part is completely optional; you can complete this part as a way to challen
 
 The following image is an example screenshot of what you should produce:
 
-<!-- 5-Advanced add tectonic plate pic here -->
+![Alt text](https://static.bc-edx.com/data/dl-1-1/m15/lms/img/5-Advanced.jpg "Earthquake and Tectonic plate map")
+
+
 Perform the following tasks:
 * Plot the tectonic plates dataset on the map in addition to the earthquakes.
 * Add other base maps to choose from.
